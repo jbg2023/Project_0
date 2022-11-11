cf45833a10c8770cfe01a004fbdeb4d03c4a76b4
jbg2023 <jbgates@protonmail.com>
2022-11-11 14:26:19 -0800

    variable for turn change on click event and callback for event listener

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 //variable for game board
 let gameBoard;
+let turn = 'X';
+
+//event listeners
+document.getElementById('board').addEventListener('click', takeTurn);
+
+function takeTurn(boxClick) {
+    let i = boxes.findIndex(function(box) {
+        return box === boxClick.targetBox;
+    });
+};
 
 //function makes array of boxes on the game board (3x3=9 boxes)
 function startGame() {
