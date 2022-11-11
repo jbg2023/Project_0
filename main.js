32bd002de2a998009b4651ab556625353c6b6b01
jbg2023 <jbgates@protonmail.com>
2022-11-11 14:01:47 -0800

    function for making a move, iteratates over board array

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ let gameBoard;
 //function makes array of boxes on the game board (3x3=9 boxes)
 function startGame() {
     gameBoard = ['', '', '', '', '', '', '', '', '',];
+    makeMove();
 };
 startGame();
+
+function makeMove() {
+    gameBoard.forEach(function(move, index){
+        console.log(move, index);
+    });
+};
