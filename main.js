851ea77ccc936d365590021a30aa465ee60843cc
jbg2023 <jbgates@protonmail.com>
2022-11-11 13:54:45 -0800

    declared variable for the game board and function to make array of boxes on board

---
+++ main.js
@@ -0,0 +1,8 @@
+//variable for game board
+let gameBoard;
+
+//function makes array of boxes on the game board (3x3=9 boxes)
+function startGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', '',];
+};
+startGame();
